a7a78cb92d64517180bd2a232fcb811ef9e52e0a
abhinab-choudhury <abhinabchoudhury291@gmail.com>
2025-02-14 14:57:06 +0530

    Basic Routing Added

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,16 @@
+import { Link } from "expo-router";
 import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
+      <Text style={styles.text}>Home</Text>
+      <Link 
+        href={"/about"}
+        style={styles.button}
+      >
+        About Page
+      </Link>
     </View>
   );
 }
@@ -19,6 +26,22 @@ const styles = StyleSheet.create({
   text: {
     color: '#fff',
     fontWeight: 'bold',
-    fontSize:20,
+    fontSize:50,
+  },
+  button: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    backgroundColor: '#4A90E2',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 3
   }
 })
